d6f908cd544be59e32cc6d87619eab846ea0c186
laewoong <simpler23@hanmail.net>
2019-07-24 00:56:51 +0900

    searchGithubReposObservable() 리팩토링.

--- app/src/main/kotlin/com/maryang/fastrxjava/ui/repos/GithubReposViewModel.kt
+++ app/src/main/kotlin/com/maryang/fastrxjava/ui/repos/GithubReposViewModel.kt
@@ -3,8 +3,7 @@ package com.maryang.fastrxjava.ui.repos
 import com.maryang.fastrxjava.data.repository.GithubRepository
 import com.maryang.fastrxjava.entity.GithubRepo
 import com.maryang.fastrxjava.util.applySchedulersExtension
-import io.reactivex.Completable
-import io.reactivex.Single
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.subjects.PublishSubject
 import java.util.concurrent.TimeUnit
@@ -29,21 +28,37 @@ class GithubReposViewModel {
             .map { it.second }
             .observeOn(AndroidSchedulers.mainThread())
 
-    fun searchGithubReposObservable() =
-        Single.create<List<GithubRepo>> { emitter ->
-            repository.searchGithubRepos(searchText)
-                .subscribe({
-                    Completable.merge(
-                        it.map { repo ->
-                            repository.checkStar(repo.owner.userName, repo.name)
-                                .doOnComplete { repo.star = true }
-                                .onErrorComplete()
-                        }
-                    ).subscribe {
-                        emitter.onSuccess(it)
-                    }
-                }, {})
-        }
+//    fun searchGithubReposObservable() =
+//        Single.create<List<GithubRepo>> { emitter ->
+//            repository.searchGithubRepos(searchText)
+//                .subscribe({
+//                    Completable.merge(
+//                        it.map { repo ->
+//                            repository.checkStar(repo.owner.userName, repo.name)
+//                                .doOnComplete { repo.star = true }
+//                                .onErrorComplete()
+//                        }
+//                    ).subscribe {
+//                        emitter.onSuccess(it)
+//                    }
+//                }, {})
+//        }
+//            .applySchedulersExtension()
+//            .toObservable()
+
+    fun searchGithubReposObservable(): Observable<List<GithubRepo>> {
+
+        fun checkStar(repo: GithubRepo) = repository.checkStar(repo.owner.userName, repo.name)
+            .doOnComplete { repo.star = true }
+            .onErrorComplete()
+            .toSingle { repo }
+
+        return repository.searchGithubRepos(searchText)
+            .flatMapObservable { Observable.fromIterable(it) }
+            .flatMapSingle { checkStar(it) }
+            .toList()
             .applySchedulersExtension()
             .toObservable()
+    }
+
 }
